1dd2214d85a845c4e8f8fa1f6cf6e6aec26bcd05
arjansunar <arjan.gahatrajsunar@gmail.com>
2022-07-08 07:40:22 +0545

    making fan colorful

---
+++ classwork/circle.c
@@ -0,0 +1,49 @@
+
+#include <math.h>
+#include <GL/glut.h>
+
+// drawing circle 
+void DrawCircle(float cx, float cy, float r) {
+    glBegin(GL_POLYGON);
+    float x,y;
+    for (int ii = 0; ii < 1000; ii++)   {
+      x= cx + r * cos(ii);
+      y= cy + r * sin(ii);
+        glVertex2f(x,y);//output vertex 
+    }
+    glEnd();
+}
+
+void display(void)
+{
+	glClear(GL_COLOR_BUFFER_BIT);
+	// Drawing two circle A
+    glColor3ub(255,255,0);        
+    DrawCircle(100,100,50);
+    glColor3ub(255,0,0);        
+    DrawCircle(100,100,35);
+
+    // Drawing two circle B
+    glColor3ub(255,255,0);        
+    DrawCircle(300,200,150);
+    glColor3ub(255,0,0);        
+    DrawCircle(300,200,105);
+
+	glFlush();
+}
+
+int main(int argc, char** argv)
+{
+    double scale = 1;
+	double width= 500;
+	double height= 500;
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_SINGLE);	// Use single display buffer.
+	glutInitWindowSize(width,height);	// Set window size.
+	glutInitWindowPosition(500, 100);
+	glutCreateWindow("fan");
+	gluOrtho2D(0, width * scale, 0, height*scale);
+	glutDisplayFunc(display);
+	glutMainLoop();
+	return 0;
+}
--- classwork/fan.c
+++ classwork/fan.c
@@ -27,6 +27,7 @@ void display(void)
 {
 	glClear(GL_COLOR_BUFFER_BIT);
 	// Draw a fan
+	glColor3d(0.5,0,0);
 	glBegin(GL_POLYGON);
 		glVertex2i(50, 20);
 		glVertex2i(70, 120);
@@ -35,6 +36,7 @@ void display(void)
 	glEnd();
 
     // diagonal
+	glColor3d(0.5,0.8,0);
     glBegin(GL_POLYGON);
 		glVertex2i(30, 80);
 		glVertex2i(20, 90);
@@ -43,6 +45,7 @@ void display(void)
 	glEnd();
 
      // diagonal
+	glColor3d(0,.9,0);
     glBegin(GL_POLYGON);
 		glVertex2i(140, 60);
 		glVertex2i(90, 110);
@@ -51,17 +54,20 @@ void display(void)
 	glEnd();
 
     // circle
+	glColor3d(0.6,0.8,0);
     DrawCircle(85, 135,15);
 
     // diagonal
+	glColor3d(0.6,0.8,0.9);
     glBegin(GL_POLYGON);
 		glVertex2i(70, 150);
 		glVertex2i(30, 190);
 		glVertex2i(40, 200);
-		glVertex2i(80, 160);
+		glVertex2i(80, 160);	
 	glEnd();
 
     // diagonal
+	glColor3ub(255,0,0);
     glBegin(GL_POLYGON);
 		glVertex2i(110, 140);
 		glVertex2i(100, 150);
@@ -81,7 +87,7 @@ int main(int argc, char** argv)
 	glutInitDisplayMode(GLUT_SINGLE);	// Use single display buffer.
 	glutInitWindowSize(width,height);	// Set window size.
 	glutInitWindowPosition(500, 100);
-	glutCreateWindow("Hello world");
+	glutCreateWindow("fan");
 	gluOrtho2D(0, width * scale, 0, height*scale);
 	glutDisplayFunc(display);
 	glutMainLoop();
